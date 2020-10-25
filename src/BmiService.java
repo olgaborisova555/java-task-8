eef17d2b60dc0260fa3af41d46b3f7fc52efc609
Nadezda Vasilyeva <nadezda.vasilyeva@gmail.com>
2020-10-25 12:30:40 +0300

    new correction

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public float calculate (int h, int w) {
-        return (float) 10000*w/(h*h);
+    public float calculate (int height, int weight) {
+        return (float) 10000*weight/(height*height);
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int height = 172;
-        int weight = 58;
-        float index = service.calculate(height, weight);
+        int newHeight = 172;
+        int newWeight = 58;
+        float index = service.calculate(newHeight, newWeight);
         System.out.printf("Ваш индекс %.1f %n", index);
     }
 }
